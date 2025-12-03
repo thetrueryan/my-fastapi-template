24cb57e2ede9127ba7459814449bafa3f0244d4e
therueryan <georgytoper@gmail.com>
2025-12-03 17:07:50 +0500

    remove alembic and sqla files from project

--- src/core/models/base.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import MetaData
-from sqlalchemy.orm import DeclarativeBase
-
-from src.core.config import settings
-
-
-class Base(DeclarativeBase):
-    metadata = MetaData(naming_convention=settings.db.convention)
