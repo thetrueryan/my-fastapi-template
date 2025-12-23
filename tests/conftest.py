946ad6313722c98d8c6f5cf8c26dea405cc03511
thetrueryan <georgytoper@gmail.com>
2025-12-24 02:48:44 +0400

    fix async client fixture in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,11 @@
 import pytest
 from httpx import AsyncClient
+from typing import AsyncGenerator
 
 from src.main import app
 
 
 @pytest.fixture
-async def ac() -> AsyncClient:
+async def ac() -> AsyncGenerator[AsyncClient, None]:
     async with AsyncClient(app=app, base_url="http://test") as client:
         yield client
